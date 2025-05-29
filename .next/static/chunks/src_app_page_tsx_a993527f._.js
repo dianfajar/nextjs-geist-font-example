066433b35476ff7d9c974808dd6e112766b53008
Dian Fajar <dianfajar@users.noreply.github.com>
2025-05-29 18:17:34 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748517452937

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@uiw_react-md-editor_esm_index_ccdb15d1.js",
+  "static/chunks/_da93a5a3._.js",
+  "static/chunks/node_modules_@uiw_d5794ec2._.css"
+],
+    source: "dynamic"
+});
